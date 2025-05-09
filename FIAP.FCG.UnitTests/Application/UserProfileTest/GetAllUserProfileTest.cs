ba54f3c5450e87ca68ce129aca26ac1726fb5449
Erik Tonin <EATTonin@apoiocdhu.sp.gov.br>
2025-05-09 13:43:32 -0300

    Unit Test For UserProfile

---
+++ FIAP.FCG.UnitTests/Application/UserProfileTest/GetAllUserProfileTest.cs
@@ -0,0 +1,81 @@
+﻿using FIAP.FCG.Application.Implementations;
+using FIAP.FCG.Domain.Contracts.IRepositories;
+using FIAP.FCG.Domain.Entities;
+using FluentAssertions;
+using Keycloak.Net;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
+
+namespace FIAP.FCG.UnitTests.Application.UserProfileTest
+{
+    public class GetAllUserProfileTest
+    {
+        private readonly Mock<IUserProfileRepository> _userProfileRepositoryMock;
+        private readonly Mock<KeycloakClient> _keycloakClientMock;
+        private readonly Mock<IOptions<KeycloakOptions>> _keycloakOptionsMock;
+        private readonly UserProfileApplicationService _userProfileService;
+
+        public GetAllUserProfileTest()
+        {
+            _keycloakOptionsMock = new Mock<IOptions<KeycloakOptions>>();
+            _keycloakOptionsMock.Setup(o => o.Value).Returns(new KeycloakOptions
+            {
+                ClientId = "meu-client-id",
+                ClientSecret = "meu-client-secret",
+                Realm = "meu-reino",
+                ServerUrl = "https://keycloak.meuservidor.com"
+            });
+
+            _userProfileRepositoryMock = new Mock<IUserProfileRepository>();
+
+            var keycloakClientMock = new Mock<KeycloakClient>("meu-client-id", "meu-client-secret");
+
+            _userProfileService = new UserProfileApplicationService(
+                keycloakClientMock.Object,
+                _userProfileRepositoryMock.Object,
+                _keycloakOptionsMock.Object
+            );
+        }
+
+        [Fact(DisplayName = nameof(GetAll_ShouldReturnAllUserProfileDTOs))]
+        [Trait("Application", "GetAll - Use Cases")]
+        public async Task GetAll_ShouldReturnAllUserProfileDTOs()
+        {
+            // Arrange
+            var users = new List<UserProfile>
+            {
+                new UserProfile("User 1", "user1@example.com", "12345678900", new DateTime(1990, 01, 01), "hashedPassword", "hashedPassword"),
+                new UserProfile("User 2", "user2@example.com", "12345678901", new DateTime(1992, 02, 02), "hashedPassword", "hashedPassword")
+            };
+
+            _userProfileRepositoryMock.Setup(repo => repo.GetAll())
+                                      .Returns(users);
+
+            // Act
+            var result = await _userProfileService.GetAll();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().HaveCount(2); // Ensure that we have 2 users returned
+            result.First().Name.Should().Be("User 1");
+            result.Last().Name.Should().Be("User 2");
+        }
+
+        [Fact(DisplayName = nameof(GetAll_UsersNotFound_ShouldThrowException))]
+        [Trait("Application", "GetAll - Use Cases")]
+        public async Task GetAll_UsersNotFound_ShouldThrowException()
+        {
+            // Arrange
+            _userProfileRepositoryMock.Setup(repo => repo.GetAll())
+                                      .Returns((IEnumerable<UserProfile>)null);
+
+            // Act
+            Func<Task> act = async () => await _userProfileService.GetAll();
+
+            // Assert
+            await act.Should().ThrowAsync<Exception>()
+                .WithMessage("Usuários não encontrados.");
+        }
+    }
+}
---
+++ FIAP.FCG.UnitTests/Application/UserProfileTest/RegisterUserProfileTest.cs
@@ -0,0 +1,174 @@
+﻿using FIAP.FCG.Application.DTOs;
+using FIAP.FCG.Application.Implementations;
+using FIAP.FCG.Domain.Contracts.IRepositories;
+using FIAP.FCG.Domain.Entities;
+using Moq;
+using Xunit;
+using Keycloak.Net;
+using FluentAssertions;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace FIAP.FCG.UnitTests.Application.UserProfileTest
+{
+    [Collection(nameof(RegisterUserProfileTest))]
+    public class RegisterUserProfileTest
+    {
+        private readonly Mock<IUserProfileRepository> _userProfileRepositoryMock;
+        private readonly Mock<KeycloakClient> _keycloakClientMock;
+        private readonly Mock<IOptions<KeycloakOptions>> _keycloakOptionsMock;
+        private readonly UserProfileApplicationService _userProfileService;
+
+        public RegisterUserProfileTest()
+        {
+            _keycloakOptionsMock = new Mock<IOptions<KeycloakOptions>>();
+            _keycloakOptionsMock.Setup(o => o.Value).Returns(new KeycloakOptions
+            {
+                ClientId = "meu-client-id",
+                ClientSecret = "meu-client-secret",
+                Realm = "meu-reino",
+                ServerUrl = "https://keycloak.meuservidor.com"
+            });
+
+            _userProfileRepositoryMock = new Mock<IUserProfileRepository>();
+
+            var keycloakClientMock = new Mock<KeycloakClient>("meu-client-id", "meu-client-secret");
+
+            _userProfileService = new UserProfileApplicationService(
+                keycloakClientMock.Object,
+                _userProfileRepositoryMock.Object,
+                _keycloakOptionsMock.Object
+            );
+        }
+
+        [Fact(DisplayName = nameof(Register_UserWithInvalidEmail_ShouldThrowException))]
+        [Trait("Application", "Register - Use Cases")]
+        public async Task Register_UserWithInvalidEmail_ShouldThrowException()
+        {
+            // Arrange
+            var userProfileDTO = new UserProfileDTO
+            {
+                Email = "invalid-email",
+                Password = "Password123!",
+                ConfirmPassword = "Password123!",
+                Name = "Test User",
+                CPF = "12345678900",
+                Birthday = new DateTime(1990, 01, 01)
+            };
+
+            // Act
+            var exception = await Assert.ThrowsAsync<Exception>(async () => await _userProfileService.Register(userProfileDTO));
+
+            //Assert
+            exception.Message.Should().Contain("E-mail com má formatação.");
+        }
+
+        [Fact(DisplayName = nameof(Register_UserAlreadyExists_ShouldAddValidationError))]
+        [Trait("Application", "Register - Use Cases")]
+        public async Task Register_UserAlreadyExists_ShouldAddValidationError()
+        {
+            // Arrange
+            var userProfileDTO = new UserProfileDTO
+            {
+                Email = "existing-email@example.com",
+                Password = "Password123!",
+                ConfirmPassword = "Password123!",
+                Name = "Test User",
+                CPF = "12345678900",
+                Birthday = new DateTime(1990, 01, 01)
+            };
+
+            var existingUser = new UserProfile(
+                "Existing User", "existing-email@example.com", "12345678900", new DateTime(1990, 01, 01), "hashedPassword", "hashedPassword"
+            );
+
+            _userProfileRepositoryMock.Setup(r => r.GetByEmail(userProfileDTO.Email))
+                                      .ReturnsAsync(existingUser);
+
+            // Act
+            var result = await _userProfileService.Register(userProfileDTO);
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+
+        [Fact(DisplayName = nameof(Register_PasswordsDoNotMatch_ShouldThrowException))]
+        [Trait("Application", "Register - Use Cases")]
+        public async Task Register_PasswordsDoNotMatch_ShouldThrowException()
+        {
+            // Arrange
+            var userProfileDTO = new UserProfileDTO
+            {
+                Email = "new-email@example.com",
+                Password = "Password123!",
+                ConfirmPassword = "DifferentPassword123!", // senhas diferentes
+                Name = "Test User",
+                CPF = "12345678900",
+                Birthday = new DateTime(1990, 01, 01)
+            };
+
+            // Act
+            var result = async () => await _userProfileService.Register(userProfileDTO);
+
+            // Assert
+            await result.Should().ThrowAsync<Exception>()
+                .WithMessage("As senhas não correspondem.");
+        }
+
+        [Fact(DisplayName = nameof(Register_InvalidPasswordFormat_ShouldThrowException))]
+        [Trait("Application", "Register - Use Cases")]
+        public async Task Register_InvalidPasswordFormat_ShouldThrowException()
+        {
+            // Arrange
+            var userProfileDTO = new UserProfileDTO
+            {
+                Email = "new-email@example.com",
+                Password = "123",
+                ConfirmPassword = "123",
+                Name = "Test User",
+                CPF = "12345678900",
+                Birthday = new DateTime(1990, 01, 01)
+            };
+
+            // Act
+            var result = async () => await _userProfileService.Register(userProfileDTO);
+
+            // Assert
+            await result.Should().ThrowAsync<Exception>()
+                .WithMessage("As senhas não estão no formato correto.");
+        }
+
+        [Fact(DisplayName = nameof(Register_SuccessfulRegistration_ShouldReturnValidResult))]
+        [Trait("Application", "Register - Use Cases")]
+        public async Task Register_SuccessfulRegistration_ShouldReturnValidResult()
+        {
+            // Arrange
+            var userProfileDTO = new UserProfileDTO
+            {
+                Email = "new-email@example.com",
+                Password = "Password123!",
+                ConfirmPassword = "Password123!",
+                Name = "Test User",
+                CPF = "12345678900",
+                Birthday = new DateTime(1990, 01, 01)
+            };
+
+            _userProfileRepositoryMock.Setup(r => r.GetByEmail(userProfileDTO.Email))
+                                      .ReturnsAsync((UserProfile)null);
+
+            _userProfileRepositoryMock.Setup(r => r.Add(It.IsAny<UserProfile>()))
+                                      .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _userProfileService.Register(userProfileDTO);
+
+            // Assert
+            result.Should().NotBeNull();
+            result.ValidationProblemDetails.Should().BeNull("não deve haver erros de validação em um cadastro bem-sucedido");
+            result.Response.Should().NotBeNull("o usuário deve ser retornado com sucesso");
+            result.Response!.Email.Should().Be(userProfileDTO.Email);
+            result.Response!.Name.Should().Be(userProfileDTO.Name);
+            _userProfileRepositoryMock.Verify(r => r.Add(It.IsAny<UserProfile>()), Times.Once);
+        }
+    }
+}
